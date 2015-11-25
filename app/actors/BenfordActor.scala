203560f49df2a0b2381449bfed3aaec99019292e
dvgodoy <dvgodoy@gmail.com>
2015-11-25 22:20:03 +0100

    Changed transformations to individual level

--- app/actors/BenfordActor.scala
+++ app/actors/BenfordActor.scala
@@ -3,7 +3,7 @@ package actors
 import akka.actor.Status.Success
 import akka.actor.{Actor, ActorLogging}
 import akka.pattern.pipe
-import com.dvgodoy.spark.benford.util.{DataByLevel, JobId, ResultsByLevel, StatsCIByLevel}
+import com.dvgodoy.spark.benford.util._
 import com.dvgodoy.spark.benford.constants.BenfordStatsDigits
 import models.BenfordCommons
 import models.BenfordService._
@@ -20,48 +20,90 @@ class BenfordActor extends Actor with ActorLogging {
   }
 
   private var data: DataByLevel = _
-  private var sampleRDD: RDD[StatsCIByLevel] = _
-  private var benfordRDD: RDD[StatsCIByLevel] = _
-  private var resultsRDD: RDD[ResultsByLevel] = _
   private var numberSamples: Int = 25000
 
+  protected var basicBoot: BasicBoot = _
+  protected var dataStatsRDD: Array[RDD[((Long, Int), StatsDigits)]] = _
+  protected var sampleRDD: Array[RDD[StatsCIByLevel]] = _
+  protected var benfordRDD: Array[RDD[StatsCIByLevel]] = _
+  protected var resultsRDD: Array[RDD[ResultsByLevel]] = _
+
   implicit val jobId = JobId(self.path.name)
+
+  def calc(groupId: Int) = {
+    if (dataStatsRDD(groupId) == null) {
+      dataStatsRDD(groupId) = BenfordCommons.calcDataStats(data, groupId)
+      sampleRDD(groupId) = BenfordCommons.calcSample(basicBoot, dataStatsRDD(groupId), data, groupId)
+      benfordRDD(groupId) = BenfordCommons.calcBenford(basicBoot, dataStatsRDD(groupId), data, groupId)
+      resultsRDD(groupId) = BenfordCommons.calcResults(sampleRDD(groupId), benfordRDD(groupId))
+    }
+  }
+
+  def calcDataStats(groupId: Int): RDD[((Long, Int), StatsDigits)] = {
+    if (dataStatsRDD(groupId) == null) {
+      dataStatsRDD(groupId) = BenfordCommons.calcDataStats(data, groupId)
+    }
+    dataStatsRDD(groupId)
+  }
+
+  def calcSample(groupId: Int): RDD[StatsCIByLevel] = {
+    if (sampleRDD(groupId) == null) {
+      sampleRDD(groupId) = BenfordCommons.calcSample(basicBoot, calcDataStats(groupId), data, groupId)
+    }
+    sampleRDD(groupId)
+  }
+
+  def calcBenford(groupId: Int): RDD[StatsCIByLevel] = {
+    if (benfordRDD(groupId) == null) {
+      benfordRDD(groupId) = BenfordCommons.calcBenford(basicBoot, calcDataStats(groupId), data, groupId)
+    }
+    benfordRDD(groupId)
+  }
+
+  def calcResults(groupId: Int): RDD[ResultsByLevel] = {
+    if (resultsRDD(groupId) == null) {
+      resultsRDD(groupId) = BenfordCommons.calcResults(calcSample(groupId), calcBenford(groupId))
+    }
+    resultsRDD(groupId)
+  }
 
   def receive = {
     case srvData(filePath: String) => {
       data = BenfordCommons.loadData(filePath)
+      val numLevels = data.levels.keys.size
+      dataStatsRDD = new Array[RDD[((Long, Int), StatsDigits)]](numLevels)
+      sampleRDD = new Array[RDD[StatsCIByLevel]](numLevels)
+      benfordRDD = new Array[RDD[StatsCIByLevel]](numLevels)
+      resultsRDD = new Array[RDD[ResultsByLevel]](numLevels)
       sender ! Success(self.path.name)
     }
     case srvCalc(numSamples: Int) => {
       numberSamples = numSamples
-      sampleRDD = BenfordCommons.calcSample(numberSamples, data)
-      benfordRDD = BenfordCommons.calcBenford(numberSamples, data)
-      resultsRDD = BenfordCommons.calcResults(sampleRDD, benfordRDD)
-      sampleRDD.cache()
-      benfordRDD.cache()
-      resultsRDD.cache()
+      basicBoot = BenfordCommons.calcBasicBoot(data, numSamples)
       sender ! Success("")
     }
     case srvNumSamples() => {
       Future(numberSamples) pipeTo sender
     }
     case srvCIsByGroupId(groupId: Int) => {
-      Future(BenfordCommons.getCIsByGroupId(sampleRDD, groupId)) pipeTo sender
+      Future(BenfordCommons.getCIsByGroupId(calcSample(groupId))) pipeTo sender
     }
     case srvCIsByLevel(level: Int) => {
-      Future(BenfordCommons.getCIsByLevel(sampleRDD, level)) pipeTo sender
+      //Future(BenfordCommons.getCIsByLevel(sampleRDD, level)) pipeTo sender
     }
     case srvBenfordCIsByGroupId(groupId: Int) => {
-      Future(BenfordCommons.getCIsByGroupId(benfordRDD, groupId)) pipeTo sender
+      calc(groupId)
+      Future(BenfordCommons.getCIsByGroupId(calcBenford(groupId))) pipeTo sender
     }
     case srvBenfordCIsByLevel(level: Int) => {
-      Future(BenfordCommons.getCIsByLevel(benfordRDD, level)) pipeTo sender
+      //Future(BenfordCommons.getCIsByLevel(benfordRDD, level)) pipeTo sender
     }
     case srvResultsByGroupId(groupId: Int) => {
-      Future(BenfordCommons.getResultsByGroupId(resultsRDD, groupId)) pipeTo sender
+      calc(groupId)
+      Future(BenfordCommons.getResultsByGroupId(calcResults(groupId))) pipeTo sender
     }
     case srvResultsByLevel(level: Int) => {
-      Future(BenfordCommons.getResultsByLevel(resultsRDD, level)) pipeTo sender
+      //Future(BenfordCommons.getResultsByLevel(resultsRDD, level)) pipeTo sender
     }
     case srvFrequenciesByGroupId(groupId: Int) => {
       Future(BenfordCommons.getFrequenciesByGroupId(data, groupId)) pipeTo sender
--- app/models/BenfordCommons.scala
+++ app/models/BenfordCommons.scala
@@ -3,7 +3,7 @@ package models
 import actors.BenfordActor
 import akka.actor.{Props, ActorSelection, ActorSystem}
 import com.dvgodoy.spark.benford.distributions.{Benford, Bootstrap}
-import com.dvgodoy.spark.benford.util.{JobId, DataByLevel, ResultsByLevel, StatsCIByLevel}
+import com.dvgodoy.spark.benford.util._
 import org.apache.spark.rdd.RDD
 import play.api.libs.json._
 
@@ -29,19 +29,25 @@ object BenfordCommons {
     SparkCommons.sc.setJobGroup(jobId.id, jobId.id)
     boot.loadData(SparkCommons.sc, filePath)(jobId)
   }
-  def calcSample(numberSamples: Int = 25000, data: DataByLevel): RDD[StatsCIByLevel] = {
-    boot.calcSampleCIs(SparkCommons.sc, data, numberSamples)
+  def calcBasicBoot(data: DataByLevel, numberSamples: Int = 25000): BasicBoot = {
+    boot.calcBasicBoot(SparkCommons.sc, data, numberSamples)
   }
-  def calcBenford(numberSamples: Int = 25000, data: DataByLevel): RDD[StatsCIByLevel] = {
-    benf.calcBenfordCIs(SparkCommons.sc, data, numberSamples)
+  def calcDataStats(data: DataByLevel, groupId: Int): RDD[((Long, Int), StatsDigits)] = {
+    boot.calcDataStats(data, groupId)
+  }
+  def calcSample(basicBoot: BasicBoot, dataStatsRDD: RDD[((Long, Int), StatsDigits)], data: DataByLevel, groupId: Int): RDD[StatsCIByLevel] = {
+    boot.calcSampleCIs(basicBoot, dataStatsRDD, data, groupId)
+  }
+  def calcBenford(basicBoot: BasicBoot, dataStatsRDD: RDD[((Long, Int), StatsDigits)], data: DataByLevel, groupId: Int): RDD[StatsCIByLevel] = {
+    benf.calcBenfordCIs(basicBoot, dataStatsRDD, data, groupId)
   }
   def calcResults(sampleRDD: RDD[StatsCIByLevel], benfordRDD: RDD[StatsCIByLevel]): RDD[ResultsByLevel] = {
     boot.calcResults(sampleRDD, benfordRDD)
   }
-  def getCIsByGroupId(sampleRDD: RDD[StatsCIByLevel], groupId: Int)(implicit jobId: JobId): JsValue = boot.getCIsByGroupId(sampleRDD, groupId)(jobId)
-  def getCIsByLevel(sampleRDD: RDD[StatsCIByLevel], level: Int)(implicit jobId: JobId): JsValue = boot.getCIsByLevel(sampleRDD, level)(jobId)
-  def getResultsByGroupId(resultsRDD: RDD[ResultsByLevel], groupId: Int)(implicit jobId: JobId): JsValue = boot.getResultsByGroupId(resultsRDD, groupId)(jobId)
-  def getResultsByLevel(resultsRDD: RDD[ResultsByLevel], level: Int)(implicit jobId: JobId): JsValue = boot.getResultsByLevel(resultsRDD, level)(jobId)
+  def getCIsByGroupId(sampleRDD: RDD[StatsCIByLevel])(implicit jobId: JobId): JsValue = boot.getCIs(sampleRDD)(jobId)
+  //def getCIsByLevel(sampleRDD: RDD[StatsCIByLevel], level: Int)(implicit jobId: JobId): JsValue = boot.getCIsByLevel(sampleRDD, level)(jobId)
+  def getResultsByGroupId(resultsRDD: RDD[ResultsByLevel])(implicit jobId: JobId): JsValue = boot.getResults(resultsRDD)(jobId)
+  //def getResultsByLevel(resultsRDD: RDD[ResultsByLevel], level: Int)(implicit jobId: JobId): JsValue = boot.getResultsByLevel(resultsRDD, level)(jobId)
   def getFrequenciesByGroupId(data: DataByLevel, groupId: Int): JsValue = boot.getFrequenciesByGroupId(data, groupId)
   def getFrequenciesByLevel(data: DataByLevel, level: Int): JsValue = boot.getFrequenciesByLevel(data, level)
   def getGroups(data: DataByLevel): JsValue = boot.getGroups(data)
